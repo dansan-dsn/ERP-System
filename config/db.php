1124cc173218ddc0dffe7a725c3b4f30b18d1edb
Dansan d.s.n <ddryn970@gmail.com>
2025-03-21 08:42:51 +0300

    temp

--- config/db.php
+++ config/db.php
@@ -1,6 +1,14 @@
 <?php
+    $host = ['local_server' => 'localhost', 'hosted_server' => ''];
+
+    $current_host = ($_SERVER['HTTP_HOST'] == 'localhost') ? $host['local_server'] : $host['hosted_server'];
+
     try {
-        $dbh = new PDO("mysql:host=localhost;dbname=expense_tracker", "root", "");
+        $dbh = new PDO("mysql:host=$current_host;dbname=expense_tracker;charset=utf8", "root", "");
     } catch (PDOException $e) {
-        print("Error: ". $e->getMessage());
+        if($current_host == "localhost") {
+            print("Error: ". $e->getMessage());
+        } else {
+            print("Database Error");
+        }
     }
--- header.php
+++ header.php
@@ -1,18 +1,62 @@
 <?php
-    include('./config/db.php')
+    include("config/db.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Expense Tracker</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <!-- ** Basic Page Needs ** -->
+  <meta charset="utf-8">
+  <title>Expense Tracker</title>
+
+  <!-- ** Mobile Specific Metas ** -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Portfolio HTML Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Html5 Portfolio Template v1.0">
+
+  <!-- Essential Stylesheets -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i|Open+Sans:400,600,700,800">
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/animate.css">
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick/slick-theme.css">
+  <link rel="stylesheet" href="plugins/themefisher-fonts/css/themefisher-fonts.min.css">  
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+  
+  <!-- Theme Stylesheet -->
+  <link rel="stylesheet" href="#" id="color-changer">
+  
+  <!--Favicon-->
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
 </head>
-<body>
-    <p style="color: green; font-family: sans-serif">Hello This is Expense tracker from PHP.</p>
-</body>
-</html>
+<body><!-- set class="dark" on body tag for DARK-THEME -->
+
+  <div class="preloader">
+    <div class="loading-mask"></div>
+    <div class="loading-mask"></div>
+    <div class="loading-mask"></div>
+    <div class="loading-mask"></div>
+    <div class="loading-mask"></div>
+  </div>
+
+  <div class="preview-wrapper">
+    <div class="switcher-head">
+      <span>Style Switcher</span>
+      <div class="switcher-trigger tf-tools"></div>
+    </div>
+
+    <div class="switcher-body">
+      <h4>Choose Color:</h4>
+      <ul class="color-options list-none">
+        <li class="c0" data-color="red" title="Default">Default</li>
+        <li class="c1" data-color="blue" title="Red">Red</li>
+        <li class="c2" data-color="green" title="Green">Green</li>
+        <li class="c3" data-color="yellow" title="Blue">Blue</li>
+      </ul>
+    </div>
+  </div>
